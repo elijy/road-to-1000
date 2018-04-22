50e91e3455b522e36a6cc771823e32e925b0350f
Elijah Joseph-Young <ejosephy@uwo.ca>
2018-04-22 15:21:27 -0400

    Set up inputs to work correctly

--- App.js
+++ App.js
@@ -1,22 +1,70 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button, TextInput } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    /* Always need this */
+    super(props);
+
+    this.state = {
+      'squat': 0,
+      'deadlift': 0,
+      'bench': 0,
+    }
+  }
+
+  updateTotal = () => {
+    console.log(this.state);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello hello World</Text>
-          <Button color="red" title="Click Me" />
+        <Text style={styles.title}>THE ROAD TO 1000</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => this.setState({'squat': text})}
+            onEndEditing={this.updateTotal}
+            placeholder={ `SQUAT (${this.state.squat})` }
+            placeholderTextColor='black'
+            keyboardType='numeric'
+          />
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => this.setState({'deadlift': text})}
+            onEndEditing={this.updateTotal}
+            placeholder='DEADLIFT'
+            placeholderTextColor='black'
+            keyboardType='numeric'
+          />
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => this.setState({'bench': text})}
+            onEndEditing={this.updateTotal}
+            placeholder='BENCH'
+            placeholderTextColor='black'
+            keyboardType='numeric'
+          />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  title: {
+    fontSize: 36,
+    fontWeight: 'bold'
+  },
+  input: {
+    width: '80%',
+    fontSize: 24,
+    marginTop: 20,
+    padding: 10
+  },
   container: {
     flex: 1,
-    backgroundColor: 'dodgerblue',
+    backgroundColor: '#f44336',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50
   },
 });
