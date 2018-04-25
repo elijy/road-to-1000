e0dcf390e6764b54116f0174df786ee33f33f510
Elijah Joseph-Young <ejosephy@uwo.ca>
2018-04-24 22:31:23 -0400

    Add persistant storage feature to app

--- App.js
+++ App.js
@@ -1,34 +1,82 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button, TextInput } from 'react-native';
+import { StyleSheet, View, Text, Button, TextInput, AsyncStorage } from 'react-native';
 
 export default class App extends React.Component {
   constructor(props) {
     /* Always need this */
     super(props);
 
+    /* Set our intial state to nothing */
     this.state = {
       'squat': 0,
       'deadlift': 0,
       'bench': 0,
       'total': ''
     }
+
+    /* GOTTA FIGURE OUT THIS SECTION */
+
+    /* Grab the total from storage */
+    AsyncStorage.getItem('total')
+      .then(value => {
+        if(value !== null) this.setState({'total': value })
+      });
+
+    /* Grab the total from storage */
+    AsyncStorage.getItem('squat')
+      .then(value => {
+        if(value !== null) this.setState({'squat': value })
+      });
+
+    /* Grab the total from storage */
+    AsyncStorage.getItem('deadlift')
+      .then(value => {
+        if(value !== null) this.setState({'deadlift': value })
+      });
+    /* Grab the total from storage */
+    AsyncStorage.getItem('bench')
+      .then(value => {
+        if(value !== null) this.setState({'bench': value })
+      });
+
   }
 
+  /* This function is used to set values into persistant storage and state */
+  setValue = (key, value) => {
+    console.log(this.state);
+
+    /* First set it into state */
+    this.setState({
+      [key]: value
+    });
+
+    /* Then add it to persistant storage */
+    try {
+      AsyncStorage.setItem(key, value.toString());
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  /* For now all the numbers we're assuming are 87% of our 1 rep max */
   calculator = (number) => {
-    return Math.round(Number(number) * (1 / 0.6));
+    return Math.round(Number(number) / 0.87);
   }
 
+  /* Here we do the actual math */
   updateTotal = () => {
     /* Grab a copy of state */
     const numbers = {...this.state};
-    console.log(numbers);
 
-    /* Calculate your 'number' based on 60% of 1 rep max */
+    /* Calculate your 'number' based on 87% of 1 rep max */
     const total =
-    this.calculator(numbers.squat) + this.calculator(numbers.deadlift) + this.calculator(numbers.bench);
+    (this.calculator(numbers.squat) + this.calculator(numbers.deadlift) + this.calculator(numbers.bench)).toString();
 
     /* Set the total */
     this.setState({total});
+
+    /* Set the total in storage */
+    this.setValue('total', total);
 
   }
 
@@ -39,7 +87,7 @@ export default class App extends React.Component {
         <Text style={styles.subtitle}>Enter your 5x5 number:</Text>
           <TextInput
             style={styles.input}
-            onChangeText={(text) => this.setState({'squat': text})}
+            onChangeText={(text) => this.setValue('squat', text)}
             onEndEditing={this.updateTotal}
             placeholder={ `SQUAT (${this.state.squat})` }
             placeholderTextColor='black'
@@ -47,17 +95,17 @@ export default class App extends React.Component {
           />
           <TextInput
             style={styles.input}
-            onChangeText={(text) => this.setState({'deadlift': text})}
+            onChangeText={(text) => this.setValue('deadlift', text)}
             onEndEditing={this.updateTotal}
-            placeholder='DEADLIFT'
+            placeholder={`DEADLIFT (${this.state.deadlift})`}
             placeholderTextColor='black'
             keyboardType='numeric'
           />
           <TextInput
             style={styles.input}
-            onChangeText={(text) => this.setState({'bench': text})}
+            onChangeText={(text) => this.setValue('bench', text)}
             onEndEditing={this.updateTotal}
-            placeholder='BENCH'
+            placeholder={`BENCH (${this.state.bench})`}
             placeholderTextColor='black'
             keyboardType='numeric'
           />
